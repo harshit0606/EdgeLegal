6d1091025a6ebd4ff50940ef26badf1ab81f6933
harshit0606 <harshitasr001@gmail.com>
2021-09-20 22:45:43 +0530

    changes

--- src/components/topStripes/homestripe.js
+++ src/components/topStripes/homestripe.js
@@ -1,14 +1,41 @@
-import React,{useEffect} from "react";
-
+import React,{useEffect,useState} from "react";
+import { useCookies } from "react-cookie";
 import "../../stylesheets/stripes.css";
 import Bell from "../../icons/bell.png";
 import { Avatar } from "@mui/material";
 import Photo from "../../icons/avatar-4.png";
-
+import axios from "axios";
+import url from "../../config.js";
 import { AiOutlineLogout } from "react-icons/ai";
 import "../../stylesheets/profileCard.css";
 function HomeStipe() {
-  
+  const [cookies, setCookie, removeCookie, get] = useCookies(["token"]);
+  const loggedInToken = cookies.token;
+  function logout() {
+    removeCookie("token");
+    window.location.href = "/";
+  }
+  const [user,setUser] = useState(null);
+  useEffect(() => {
+    axios
+      .get(
+        `${url}/api/user/${1}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${loggedInToken}`,
+          },
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        console.log("userdetails",response.data);
+        setUser(response.data);
+      });
+  }, []);
+
   
 function showProfile(){
     const profile = document.querySelector(".profilecard");
@@ -41,24 +68,25 @@ function showProfile(){
         <div className="avatarDiv">
           <Avatar sx={{ width: 56, height: 56 }} src={Photo} />
           <div className="nameDiv">
-            <h3>Sierra Ferguson</h3>
-            <p>testuser1@gmail.com</p>
+            <h3>{user?.firstName} {user?.lastName}</h3>
+            <p>{user?.login}</p>
           </div>
         </div>
         <div className="cardDiv">
-          <h3>Company Location</h3>
-          <p>Warren and Warren - Bangalore</p>
+          <h3>Company Name</h3>
+          <p>{user?.organizationName} </p>
           <h3>Role</h3>
           <p>System administrator</p>
           <h3>Version</h3>
           <p>2.0.102</p>
           <hr></hr>
           <h3>My profile</h3>
-          <p>Warren and Warren - Bangalore</p>
-          <p>Warren and Warren - Bangalore</p>
-          <p>Warren and Warren - Bangalore</p>
-          <p>Warren and Warren - Bangalore</p>
-          <div className="logoutdivv">
+          {user?.siteInfoList.map((site)=>{
+            return <p>{site.siteName}</p>
+          })}
+          <div onClick={() => {
+            logout();
+          }} className="logoutdivv">
             <h2>
               Logout <AiOutlineLogout />
             </h2>
