b638577829b98c30dd6fb00ba667bdebc9cd1cd1
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2021-10-11 19:01:35 +0530

    few changes

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -8,13 +8,12 @@ import contacts from '../icons/contacts.png';
 import { Link } from 'react-router-dom';
 import '../stylesheets/sidebar.css';
 function Sidebar(props) {
-
   function handleCollapse(e) {
     const mainSidebar = document.querySelector('#side');
-    const minisidebar = document.querySelector('.minisidebar');
-    
+    // const minisidebar = document.querySelector('.minisidebar');
+
     e.preventDefault();
-    if (props.collapse == false) {
+    if (props.collapse === false) {
       if (mainSidebar) mainSidebar.classList.add('collapsse');
       props.setCollapse(true);
     } else {
@@ -27,68 +26,68 @@ function Sidebar(props) {
     <div>
       <div id='side' className='sidebarDiv'>
         <div className='logo_div'>
-          <img src={Ham} onClick={handleCollapse} />
+          <img src={Ham} alt='toggle' onClick={handleCollapse} />
           <h1>Edge</h1>
         </div>
         <Link to='/home/matters' style={{ textDecoration: 'none' }}>
           <div className='firstTab' onClick={props.setCurrent('matters')}>
-            <img src={Matters} />
+            <img src={Matters} alt='matters' />
             <p>Matters</p>
           </div>
         </Link>
         <div className='sideTab'>
-          <img src={documentt} />
+          <img src={documentt} alt='doc' />
           <p>Document</p>
         </div>
         <div className='sideTab'>
-          <img src={task} />
+          <img src={task} alt='tasks' />
           <p>Tasks</p>
         </div>
         <Link to='/home/contacts' style={{ textDecoration: 'none' }}>
           <div className='sideTab' onClick={props.setCurrent('contacts')}>
-            <img src={contacts} />
+            <img src={contacts} alt='contacts' />
             <p>Contacts</p>
           </div>
         </Link>
         <Link to='/home/safecustody' style={{ textDecoration: 'none' }}>
           <div className='sideTab' onClick={props.setCurrent('safeCustody')}>
-            <img src={Safe} />
+            <img src={Safe} alt='safecustody' />
             <p>Safe Custody</p>
           </div>
         </Link>
         <Link to='/home/property' style={{ textDecoration: 'none' }}>
           <div className='sideTab' onClick={props.setCurrent('safeCustody')}>
-            <img src={Safe} />
+            <img src={Safe} alt='property' />
             <p>Property</p>
           </div>
         </Link>
       </div>
       <div className='minisidebar'>
-        <img src={Ham} onClick={handleCollapse} />
+        <img src={Ham} onClick={handleCollapse} alt='miniToggle' />
         <Link to='/home/matters' style={{ textDecoration: 'none' }}>
           <div className='firstminiTab'>
-            <img src={Matters} />
+            <img src={Matters} alt='matters' />
           </div>
         </Link>
         <div className='minisideTab'>
-          <img src={documentt} />
+          <img src={documentt} alt='doc' />
         </div>
         <div className='minisideTab'>
-          <img src={task} />
+          <img src={task} alt='tasks' />
         </div>
         <Link to='/home/contacts' style={{ textDecoration: 'none' }}>
           <div className='minisideTab'>
-            <img src={contacts} />
+            <img src={contacts} alt='contacts' />
           </div>
         </Link>
         <Link to='/home/safecustody' style={{ textDecoration: 'none' }}>
           <div className='minisideTab'>
-            <img src={Safe} />
+            <img src={Safe} alt='safecustody' />
           </div>
         </Link>
         <Link to='/home/property' style={{ textDecoration: 'none' }}>
           <div className='minisideTab'>
-            <img src={Safe} />
+            <img src={Safe} alt='property' />
           </div>
         </Link>
       </div>
--- src/components/topStripes/homestripe.js
+++ src/components/topStripes/homestripe.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
-import "../../stylesheets/stripes.css";
-import Bell from "../../icons/bell.png";
-import { Avatar } from "@mui/material";
-import Photo from "../../icons/avatar-4 (1).png";
-import axios from "axios";
-import url from "../../config.js";
-import { AiOutlineLogout } from "react-icons/ai";
-import { FiEdit } from "react-icons/fi";
-import { Modal, Button } from "react-bootstrap";
-import "../../stylesheets/profileCard.css";
-import{MdSearch} from "react-icons/md";
-import companyLogo from "../../icons/edgelogo.png"
+import React, { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
+import '../../stylesheets/stripes.css';
+import Bell from '../../icons/bell.png';
+import { Avatar } from '@mui/material';
+import Photo from '../../icons/avatar-4 (1).png';
+import axios from 'axios';
+import url from '../../config.js';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { FiEdit } from 'react-icons/fi';
+import { Modal } from 'react-bootstrap';
+import '../../stylesheets/profileCard.css';
+import { MdSearch } from 'react-icons/md';
+import companyLogo from '../../icons/edgelogo.png';
 function HomeStipe() {
-  const [cookies, setCookie, removeCookie, get] = useCookies(["token"]);
+  const [cookies, setCookie, removeCookie] = useCookies(['token']);
   const loggedInToken = cookies.token;
   function logout() {
-    removeCookie("token");
-    window.location.href = "/";
+    removeCookie('token');
+    window.location.href = '/';
   }
   const [user, setUser] = useState(null);
   const [show, setShow] = useState(false);
@@ -27,7 +27,7 @@ function HomeStipe() {
         `${url}/api/user/${1}`,
         {
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
             Authorization: `Bearer ${loggedInToken}`,
           },
         },
@@ -36,20 +36,20 @@ function HomeStipe() {
         }
       )
       .then((response) => {
-        console.log("userdetails", response.data);
+        console.log('userdetails', response.data);
         setUser(response.data);
       });
   }, []);
 
   function showProfile() {
-    const profile = document.querySelector(".profilecard");
-    const backdrop = document.querySelector(".backdroppp");
+    const profile = document.querySelector('.profilecard');
+    const backdrop = document.querySelector('.backdroppp');
 
     if (profile) {
-      profile.classList.toggle("show");
+      profile.classList.toggle('show');
     }
     if (backdrop) {
-      backdrop.classList.toggle("show");
+      backdrop.classList.toggle('show');
     }
   }
 
@@ -61,38 +61,48 @@ function HomeStipe() {
   };
 
   return (
-    <div className="homestripe">
-    <div className="backdroppp" onClick={showProfile}></div>
-      <div className="safestrip">
-      <div className="searchhdiv">
-          <input style={{outline:'none'}} placeholder="Search" />
-          <div style={{backgroundColor:"lightgray",width:"fit-content",padding:"2px 2px",cursor:"pointer"}}>
-          <MdSearch size={25}/></div>
+    <div className='homestripe'>
+      <div className='backdroppp' onClick={showProfile}></div>
+      <div className='safestrip'>
+        <div className='searchhdiv'>
+          <input style={{ outline: 'none' }} placeholder='Search' />
+          <div
+            style={{
+              backgroundColor: 'lightgray',
+              width: 'fit-content',
+              padding: '2px 2px',
+              cursor: 'pointer',
+            }}
+          >
+            <MdSearch size={25} />
+          </div>
         </div>
-      
-        <div className="logo_divv"><img src={companyLogo}/></div>
-          <div className="safe_iconsDiv">
-          <img className="safe_iconsDivimg" src={Bell} />
-          <div onClick={showProfile} className="avatarr">
+
+        <div className='logo_divv'>
+          <img src={companyLogo} alt='company' />
+        </div>
+        <div className='safe_iconsDiv'>
+          <img className='safe_iconsDivimg' src={Bell} alt='notifications' />
+          <div onClick={showProfile} className='avatarr'>
             <Avatar sx={{ width: 56, height: 56 }} src={Photo} />
           </div>
         </div>
       </div>
-      <div className="profilecard">
+      <div className='profilecard'>
         <div
           onClick={handleShow}
           style={{
-            position: "absolute",
-            top: "10px",
-            right: "10px",
-            cursor: "pointer",
+            position: 'absolute',
+            top: '10px',
+            right: '10px',
+            cursor: 'pointer',
           }}
         >
           <FiEdit />
         </div>
-        <div className="avatarDiv">
+        <div className='avatarDiv'>
           <Avatar sx={{ width: 56, height: 56 }} src={Photo} />
-          <div className="nameDiv">
+          <div className='nameDiv'>
             <h3>
               {user?.firstName} {user?.lastName}
             </h3>
@@ -100,34 +110,53 @@ function HomeStipe() {
           </div>
           <Modal show={show} onHide={handleClose}>
             <Modal.Body>
-              <div className="editprofileDiv">
-
+              <div className='editprofileDiv'>
                 <h3>Change Password</h3>
-                <p style={{fontSize:"20px" ,cursor:"pointer",position:"absolute",top:"10px",right:"20px"}} onClick={handleClose}>&#10006;</p>
-                <div className="editnamediv">
-                <div class="row">
-                    <div class="col-12">
-                  <input className="editProfile-input" placeholder="Current Password"/></div> 
-                </div>
-                <div class="row">
-                <div class="col-12">
-                <input className="editProfile-input" placeholder="New Password" /><br/></div>
-               
-                </div>
-                  <div class="row">
-                  <div class="col-12">
-                  <input className="editProfile-input" placeholder="Confirm Password"/>
+                <p
+                  style={{
+                    fontSize: '20px',
+                    cursor: 'pointer',
+                    position: 'absolute',
+                    top: '10px',
+                    right: '20px',
+                  }}
+                  onClick={handleClose}
+                >
+                  &#10006;
+                </p>
+                <div className='editnamediv'>
+                  <div class='row'>
+                    <div class='col-12'>
+                      <input
+                        className='editProfile-input'
+                        placeholder='Current Password'
+                      />
+                    </div>
                   </div>
-                  
+                  <div class='row'>
+                    <div class='col-12'>
+                      <input
+                        className='editProfile-input'
+                        placeholder='New Password'
+                      />
+                      <br />
+                    </div>
+                  </div>
+                  <div class='row'>
+                    <div class='col-12'>
+                      <input
+                        className='editProfile-input'
+                        placeholder='Confirm Password'
+                      />
+                    </div>
+                  </div>
+                  <button className='EditProfileBtn'>Update</button>
                 </div>
-                <button className="EditProfileBtn">Update</button>
-             
-              </div>
               </div>
             </Modal.Body>
           </Modal>
         </div>
-        <div className="cardDiv">
+        <div className='cardDiv'>
           <h3>Company Name</h3>
           <p>{user?.organizationName} </p>
           <h3>Role</h3>
@@ -143,7 +172,7 @@ function HomeStipe() {
             onClick={() => {
               logout();
             }}
-            className="logoutdivv"
+            className='logoutdivv'
           >
             <h2>
               Logout <AiOutlineLogout />
