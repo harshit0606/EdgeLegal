93cb726635c29a9d57038c1893deb0a6587f3423
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2021-10-26 20:08:45 +0530

    pagination and other changes

--- src/components/contacts/OrganisationDetail.js
+++ src/components/contacts/OrganisationDetail.js
@@ -1,96 +1,26 @@
 import React from 'react';
 import moment from 'moment';
 import '../../stylesheets/contact-details.css';
+import EditOrgDetails from './EditOrgDetails';
 
 function OrganisationDetail(props) {
-  const { contactDetails } = props;
+  const {
+    contactDetails,
+    changeBool,
+    allCountries,
+    setUpdatedData,
+    updatedData,
+  } = props;
   // console.log(contactDetails);
   return (
     <div className='main-contact-detail-div'>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Name</p>
-          <h3>{contactDetails?.name ? contactDetails?.name : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Legal Name</p>
-          <h3>{contactDetails?.legalName ? contactDetails?.legalName : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Title</p>
-          <h3>{contactDetails?.title ? contactDetails?.title : ''}</h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Sub-type</p>
-          <h3>{contactDetails?.subType ? contactDetails?.subType : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>ABN</p>
-          <h3>{contactDetails?.abn ? contactDetails?.abn : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>ACN</p>
-          <h3>{contactDetails?.acn ? contactDetails?.acn : ''}</h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Email Address</p>
-          <h3>{contactDetails?.emailId1 ? contactDetails?.emailId1 : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Email Address</p>
-          <h3>{contactDetails?.emailId2 ? contactDetails?.emailId2 : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Fax Number</p>
-          <h3>{contactDetails?.faxNumber ? contactDetails?.faxNumber : ''}</h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Mobile Number</p>
-          <h3>
-            {contactDetails?.mobilePhoneNumber
-              ? contactDetails?.mobilePhoneNumber
-              : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Phone Number</p>
-          <h3>
-            {contactDetails?.phoneNumber1 ? contactDetails?.phoneNumber1 : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Phone Number</p>
-          <h3>
-            {contactDetails?.phoneNumber2 ? contactDetails?.phoneNumber2 : ''}
-          </h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Communication Address</p>
-          <h3>
-            {contactDetails?.commAddress1
-              ? `${contactDetails?.commAddress1}, ${contactDetails?.commAddress2} ${contactDetails?.commAddress3}`
-              : ''}
-          </h3>
-        </div>
-
-        <div className='smaller-detail-div'>
-          <p>Mailing Address</p>
-          <h3>
-            {contactDetails?.commAddress1
-              ? `${contactDetails?.mailingAddress1}, ${contactDetails?.mailingAddress2} ${contactDetails?.mailingAddress3}`
-              : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'></div>
-      </div>
+      <EditOrgDetails
+        contactDetails={contactDetails}
+        changeBool={changeBool}
+        allCountries={allCountries}
+        setUpdatedData={setUpdatedData}
+        updatedData={updatedData}
+      />
     </div>
   );
 }
--- src/components/contacts/PersonDetail.js
+++ src/components/contacts/PersonDetail.js
@@ -1,142 +1,25 @@
 import React from 'react';
 import moment from 'moment';
 import '../../stylesheets/contact-details.css';
+import EditPersonDetails from './EditPersonDetails';
 function PersonDetail(props) {
-  const { contactDetails } = props;
+  const {
+    contactDetails,
+    changeBool,
+    allCountries,
+    setUpdatedData,
+    updatedData,
+  } = props;
   // console.log(contactDetails);
   return (
     <div className='main-contact-detail-div'>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Status</p>
-          <h3>
-            {contactDetails?.flagDeactivated === 1 ? 'De-Active' : 'Active'}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Type</p>
-          <h3>{contactDetails?.role ? contactDetails?.role : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Salutaion</p>
-          <h3>
-            {contactDetails?.salutation ? contactDetails?.salutation : ''}
-          </h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>First Name</p>
-          <h3>{contactDetails?.firstName ? contactDetails?.firstName : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Middle Name</p>
-          <h3>
-            {contactDetails?.middleName ? contactDetails?.middleName : ''}
-          </h3>
-        </div>
-
-        <div className='smaller-detail-div'>
-          <p>Last Name</p>
-          <h3>{contactDetails?.lastName ? contactDetails?.lastName : ''}</h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Country of Brith</p>
-          <h3>
-            {contactDetails?.countryOfBirth
-              ? contactDetails?.countryOfBirth
-              : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Date of Birth</p>
-          <h3>
-            {contactDetails?.dateOfBirth
-              ? moment(contactDetails?.dateOfBirth).format('DD-MM-YYYY')
-              : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Place of birth</p>
-          <h3>
-            {contactDetails?.placeOfBirth ? contactDetails?.placeOfBirth : ''}
-          </h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Gender</p>
-          <h3>{contactDetails?.gender ? contactDetails?.gender : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Email 1</p>
-          <h3>{contactDetails?.emailId1 ? contactDetails?.emailId1 : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Email 2</p>
-          <h3>{contactDetails?.emailId2 ? contactDetails?.emailId2 : ''}</h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Home Phone</p>
-          <h3>
-            {contactDetails?.phoneNumber1 ? contactDetails?.phoneNumber1 : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Mobile Number</p>
-          <h3>
-            {contactDetails?.mobilePhoneNumber
-              ? contactDetails?.mobilePhoneNumber
-              : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Fax Number</p>
-          <h3>{contactDetails?.faxNumber ? contactDetails?.faxNumber : ''}</h3>
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Passport Number</p>
-          <h3>
-            {contactDetails?.passportNumber
-              ? contactDetails?.passportNumber
-              : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'>
-          <p>Website</p>
-          <h3>{contactDetails?.website ? contactDetails?.website : ''}</h3>
-        </div>
-        <div className='smaller-detail-div'>
-          {/**<p>Comment</p>
-            <h3>{'comment'}</h3> */}
-        </div>
-      </div>
-      <div className='contact-detail-horizontal'>
-        <div className='smaller-detail-div'>
-          <p>Communication Address</p>
-          <h3>
-            {contactDetails?.commAddress1
-              ? `${contactDetails?.commAddress1}, ${contactDetails?.commAddress2} ${contactDetails?.commAddress3}`
-              : ''}
-          </h3>
-        </div>
-
-        <div className='smaller-detail-div'>
-          <p>Mailing Address</p>
-          <h3>
-            {contactDetails?.commAddress1
-              ? `${contactDetails?.mailingAddress1}, ${contactDetails?.mailingAddress2} ${contactDetails?.mailingAddress3}`
-              : ''}
-          </h3>
-        </div>
-        <div className='smaller-detail-div'></div>
-      </div>
+      <EditPersonDetails
+        contactDetails={contactDetails}
+        changeBool={changeBool}
+        allCountries={allCountries}
+        setUpdatedData={setUpdatedData}
+        updatedData={updatedData}
+      />
     </div>
   );
 }
