33c255a77707b0170da4efd15de1f55a89995d7b
Sunil C <sunil.chanchlani@gmail.com>
2021-11-01 08:53:28 +0530

    Updated config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // const url = "http://ec2-35-154-158-113.ap-south-1.compute.amazonaws.com:5000";
 // const url = "http://ec2-13-233-68-230.ap-south-1.compute.amazonaws.com:5000";
 // const url = "http://ec2-13-233-68-230.ap-south-1.compute.amazonaws.com:5000";
-const url = 'http://ec2-52-66-208-198.ap-south-1.compute.amazonaws.com:5000';
+//const url = 'http://ec2-52-66-208-198.ap-south-1.compute.amazonaws.com:5000';
+const url = 'http://ec2-13-233-123-174.ap-south-1.compute.amazonaws.com:5000';
 
 export default url;
